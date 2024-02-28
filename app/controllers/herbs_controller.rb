cefa23f18cf5f2b6e150d6d262a1586bfefe9329
chantelcarter <chantel.alexia@gmail.com>
2024-02-28 09:51:55 -0800

    index controller method and rspec testing

--- app/controllers/herbs_controller.rb
+++ app/controllers/herbs_controller.rb
@@ -1,2 +1,6 @@
 class HerbsController < ApplicationController
+  def index
+    herbs = Herb.all
+    render json: herbs
+  end
 end
--- spec/requests/herbs_spec.rb
+++ spec/requests/herbs_spec.rb
@@ -1,7 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe "Herbs", type: :request do
+  let(:user) { User.create(
+    email: 'test@example.com',
+    password: 'password',
+    password_confirmation: 'password'
+    )
+  }
+
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'gets a list of herbs' do 
+      herb = user.herbs.create(
+        name: "Blue Lotus Root",
+        scientific_name: "Nymphaea caerulea",
+        summary: "Also known as the Sacred Blue Lily, it's benefits have laid claims for thousands of years as a sleep aid, aphrodisiac and much more while it boasts having psychoactive properties.",
+        benefit: "Improves Sleep, Eases Anxiety and Aphrodisiac",
+        warning: "Psychoactive properties should not be mixes with other substances",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Lotus_india_cropped.jpg/1200px-Lotus_india_cropped.jpg"
+      )
+
+      get '/herbs'
+
+      herb = JSON.parse(response.body)
+      expect(herb.first['name']).to eq("Blue Lotus Root")
+      expect(herb.first['scientific_name']).to eq("Nymphaea caerulea")
+      expect(herb.first['summary']).to eq("Also known as the Sacred Blue Lily, it's benefits have laid claims for thousands of years as a sleep aid, aphrodisiac and much more while it boasts having psychoactive properties.")
+      expect(herb.first['benefit']).to eq("Improves Sleep, Eases Anxiety and Aphrodisiac")
+      expect(herb.first['warning']).to eq("Psychoactive properties should not be mixes with other substances")
+      expect(herb.first['image']).to eq("https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Lotus_india_cropped.jpg/1200px-Lotus_india_cropped.jpg")
+      expect(response).to have_http_status(200)
+    end
   end
 end
